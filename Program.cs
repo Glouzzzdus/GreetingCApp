567c89fdb247d3182b6f366f582428c8ce4061dc
Andrii_Hluzdovskyi <Andrii_Hluzdovskyi@EPUALVIW05BD>
2023-10-10 08:09:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using GreetingLib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            var username = args[0];
+            var message = GreetingGenerator.GenerateGreeting(username);
+
+            Console.WriteLine(message);
+        }
+        else
+        {
+            Console.WriteLine("Please provide a username as an argument.");
+        }
+    }
+}
